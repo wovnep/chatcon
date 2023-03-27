1c8eba88584e651bec2fd1a20ddb9b817f38f737
wovnep <122073572+wovnep@users.noreply.github.com>
2023-03-27 12:27:49 +0000

    frontend with test example

---
+++ ui/src/components/InputArea.tsx
@@ -0,0 +1,5 @@
+export default function InputArea() {
+    return (
+        <textarea id="input-area" className="input-area" placeholder="Send a message" />
+    )
+}
---
+++ ui/src/lib/example.chatEvent.ts
@@ -0,0 +1,7 @@
+export const getNewMessage = (i:number) => {
+    return {
+        badge: ["https://static-cdn.jtvnw.net/badges/v1/ad6a380a-1cbe-4d63-bb22-3f2843f7d243/3", "https://static-cdn.jtvnw.net/badges/v1/fe5b5ddc-93e7-4aaf-9b3e-799cd41808b1/1", "https://cdn.7tv.app/badge/62f97c05e46eb00e438a696a/1x"],
+        username: "xQc",
+        message: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, eaque voluptatem. Culpa eveniet officia et praesentium ad commodi quia, accusantium eaque ab, suscipit incidunt pariatur. Iure neque dolorem iusto facere ${i}`,
+    }
+}
